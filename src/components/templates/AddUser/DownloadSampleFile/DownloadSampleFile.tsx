ea46e9ca9b7c4bd85a07d144ab0a052ccf8328e6
one-odin <one.odin@yahoo.com>
2024-10-27 13:09:03 +0330

    change sample file download file

--- src/components/templates/AddUser/DownloadSampleFile/DownloadSampleFile.tsx
+++ src/components/templates/AddUser/DownloadSampleFile/DownloadSampleFile.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../../../modules/Button/Button";
 import { HiOutlineDocumentDownload } from "react-icons/hi";
 
@@ -5,10 +6,12 @@ const DownloadSampleFile = (): JSX.Element => {
   return (
     <div className="flex justify-between items-center mb-3">
       <p>لطفا قالب نمونه اکسل را دانلود و پس از تکمیل آن را بارگزاری کنید.</p>
-      <Button variant="primaryOutline" size="medium" icon={true}>
-        <HiOutlineDocumentDownload className="ml-2 size-5 -mt-2" />
-        <span>دانلود قالب آماده اکسل</span>
-      </Button>
+      <Link to="/src/assets/users-excel-file.xlsx" target="_blank">
+        <Button variant="primaryOutline" size="medium" icon={true}>
+          <HiOutlineDocumentDownload className="ml-2 size-5 -mt-2" />
+          <span>دانلود قالب آماده اکسل</span>
+        </Button>
+      </Link>
     </div>
   );
 };
